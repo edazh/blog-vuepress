8ed9ca636d5adbf0811d1535e3b61582165bc911
edazh <edazh@qq.com>
2019-04-29 02:19:18 +0800

    更新简历

--- sites/.vuepress/config.js
+++ sites/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: '益达的博客',
   description: '有一些事情需要记录，会写在这里吧',
+  base: '/blog-vuepress/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
       { text: '博客', link: '/blog/' },
       { text: '踩坑手册', link: '/workbook/' },
-      { text: '简历', link: '/resume/' },
       { text: 'Github', link: 'https://github.com/edazh/' },
     ],
     sidebar: {
@@ -18,13 +18,8 @@ module.exports = {
         },
       ],
       '/workbook/': [''],
-
       // fallback
-      '/': [
-        '' /* / */,
-        'blog' /* /contact.html */,
-        'workbook' /* /about.html */,
-      ],
+      '/': [],
     },
   },
 }
